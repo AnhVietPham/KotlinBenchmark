52c7edefc213940786a4d081b82364e6df158967
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-22 08:43:54 +0700

    Update more information of JMH

--- src/main/kotlin/inlinefunction/InlineBenchmark.kt
+++ src/main/kotlin/inlinefunction/InlineBenchmark.kt
@@ -8,6 +8,8 @@ import java.util.concurrent.TimeUnit
 /*
 * https://www.programmersought.com/article/4721643238/
 * http://tutorials.jenkov.com/java-performance/index.html
+* https://stackoverflow.com/questions/47776345/what-is-the-difference-between-warmup-attribute-in-fork-and-warmup-annotation-in
+* https://blog.avenuecode.com/java-microbenchmarks-with-jmh-part-1
 * */
 fun nonInlineRepeat(times: Int, action: (Int) -> Unit) {
     for (index in 0 until times) {
